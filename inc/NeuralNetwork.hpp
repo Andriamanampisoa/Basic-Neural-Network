03a24454c8c8cf9c491f58f66cdb4f6b21f037cd
Andriamanampisoa <toavina.andriamanampisoa@epitech.eu>
2026-05-20 16:21:28 +0900

    refactor: for the pull request

--- inc/NeuralNetwork.hpp
+++ inc/NeuralNetwork.hpp
@@ -1,5 +1,5 @@
 /*
- * Filename: /home/utilisateur/Projects/Basic-Neural-Network/inc/NeuralNetwork.hpp
+ * Filename: Basic-Neural-Network/inc/NeuralNetwork.hpp
  * Created Date: Wednesday, May 20th 2026, 3:51:20 pm
  * Author: Toa
  * Description: Header file for the Neural Network class
@@ -11,19 +11,19 @@
  * @file NeuralNetwork.hpp
  * @brief Header file for the Neural Network class
  */
-class NNetwork
+class NeuralNetwork
 {
     public:
 
     /**
      * @brief Constructor for the Neural Network class
      */
-    NNetwork();
+    NeuralNetwork();
 
     /**
      * @brief Destructor for the Neural Network class
      */
-    ~NNetwork() = default;
+    ~NeuralNetwork() = default;
 
     private:
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 /*
- * Filename: /home/utilisateur/Projects/Basic-Neural-Network/src/main.cpp
+ * Filename: Basic-Neural-Network/src/main.cpp
  * Created Date: Wednesday, May 20th 2026, 3:38:28 pm
  * Author: Toa
  * Description: Main entry point for the neural network application
