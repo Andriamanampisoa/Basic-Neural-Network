c120a1fe78bff148cc2d1904995626fdc10381e1
Andriamanampisoa <toavina.andriamanampisoa@epitech.eu>
2026-05-20 16:11:11 +0900

    task: add header file and implementation file for the NNetwork class

---
+++ inc/NeuralNetwork.hpp
@@ -0,0 +1,29 @@
+/*
+ * Filename: /home/utilisateur/Projects/Basic-Neural-Network/inc/NeuralNetwork.hpp
+ * Created Date: Wednesday, May 20th 2026, 3:51:20 pm
+ * Author: Toa
+ * Description: Header file for the Neural Network class
+*/
+
+#pragma once
+
+/**
+ * @file NeuralNetwork.hpp
+ * @brief Header file for the Neural Network class
+ */
+class NNetwork
+{
+    public:
+
+    /**
+     * @brief Constructor for the Neural Network class
+     */
+    NNetwork();
+
+    /**
+     * @brief Destructor for the Neural Network class
+     */
+    ~NNetwork() = default;
+
+    private:
+};
---
+++ src/NeuralNetwork.cpp
@@ -0,0 +1,6 @@
+/*
+ * Filename: /home/utilisateur/Projects/Basic-Neural-Network/src/NeuralNetwork.cpp
+ * Created Date: Wednesday, May 20th 2026, 3:51:28 pm
+ * Author: Toa
+ * Description: Implementation file for the Neural Network class
+*/
