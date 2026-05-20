886050669dcc259bcd593234ffd0bdfe1376db75
Andriamanampisoa <toavina.andriamanampisoa@epitech.eu>
2026-05-20 16:12:36 +0900

    task: add basic main

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+/*
+ * Filename: /home/utilisateur/Projects/Basic-Neural-Network/src/main.cpp
+ * Created Date: Wednesday, May 20th 2026, 3:38:28 pm
+ * Author: Toa
+ * Description: Main entry point for the neural network application
+*/
+
+#include <iostream>
+
+/**
+ * @file main.cpp
+ * @brief Main entry point for the neural network application
+ */
+int main()
+{
+    std::cout << "Hello World" << std::endl;
+}
