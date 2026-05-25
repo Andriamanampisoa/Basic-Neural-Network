7f92c9ef8c76054544f5583dad44b0127b4e94e0
Andriamanampisoa <toavina.andriamanampisoa@epitech.eu>
2026-05-25 16:51:36 +0900

    task: add types.hpp file and add the 'Layer' typedef

---
+++ inc/types.hpp
@@ -0,0 +1,12 @@
+/*
+ * Filename: Basic-Neural-Network/inc/types.hpp
+ * Created Date: Monday, May 25th 2026, 4:32:22 pm
+ * Author: Toa
+ * Description: Header file for the types
+*/
+
+#pragma once
+
+#include <vector>
+
+typedef std::vector<double> Layer;
