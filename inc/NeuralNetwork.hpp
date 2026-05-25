556050cd902b17f412d23876072c0b2593d8b073
Andriamanampisoa <toavina.andriamanampisoa@epitech.eu>
2026-05-25 16:52:55 +0900

    task: modified NeuralNetwork class's constructor and add new functions prototypes to the class

--- inc/NeuralNetwork.hpp
+++ inc/NeuralNetwork.hpp
@@ -7,6 +7,8 @@
 
 #pragma once
 
+#include "types.hpp"
+
 /**
  * @file NeuralNetwork.hpp
  * @brief Header file for the Neural Network class
@@ -16,14 +18,33 @@ class NeuralNetwork
     public:
 
     /**
-     * @brief Constructor for the Neural Network class
+     * @brief Constructor for the Neural Network class.
+     * @details The topology is a vector of the number of neurons in each layer.
+     * The first layer is the input layer, the last layer is the output layer, and the layers in between are the hidden layers.
+     * Each layer has a bias neuron.
+     * @param topology The topology of the Neural Network
      */
-    NeuralNetwork();
+    NeuralNetwork(const std::vector<unsigned int> &topology);
 
     /**
-     * @brief Destructor for the Neural Network class
+     * @brief Forward propagation for the Neural Network class
+     * @param inputValues The input values for the forward propagation
      */
-    ~NeuralNetwork();
+    void forwardPropagation(const std::vector<double> &inputValues);
+
+    /**
+     * @brief Back propagation for the Neural Network class
+     * @param expectedValues The expected values for the back propagation
+     */
+    void backPropagation(const std::vector<double> &expectedValues);
+
+    /**
+     * @brief Back propagation for the Neural Network class
+     * @param resultsValues The results values for the back propagation
+     */
+    void getResults(std::vector<double> &resultsValues) const;
 
     private:
+
+    std::vector<Layer> __layer;
 };
