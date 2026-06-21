1d56601695ac259faea75865e2e8bf8a09d77539
Andriamanampisoa <toavina.andriamanampisoa@epitech.eu>
2026-06-21 17:51:05 +0900

    refactor: add the Neuron type to the layer init

--- src/NeuralNetwork.cpp
+++ src/NeuralNetwork.cpp
@@ -10,11 +10,13 @@
 NeuralNetwork::NeuralNetwork(const std::vector<unsigned int> &topology)
 {
     unsigned int numLayers = topology.size();
+    unsigned int numOutputs = 0;
 
     for (unsigned int layerNum = 0; layerNum < numLayers; layerNum++) {
         _layer.push_back(Layer());
+        numOutputs = layerNum == topology.size() - 1 ? 0 : topology[layerNum + 1];
         for (unsigned int neuronNum = 0; neuronNum <= topology[layerNum]; neuronNum++) {
-            _layer.back().push_back(0.0);
+            _layer.back().push_back(Neuron(numOutputs));
         }
     }
 }
